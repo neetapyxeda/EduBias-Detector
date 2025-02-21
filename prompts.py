da5bb0073f0a9cd26403a4d0227daf07bf536cf5
Shilpi2525 <shilpi.shaw@pyxeda.ai>
2025-02-21 17:33:01 +0530

    Update prompts.py

--- prompts.py
+++ prompts.py
@@ -1,13 +1,13 @@
-BIAS_ANALYSER = """
-You are very good at analysing bias and you are tasked with analyzing a text to identify potential biases.
+BIAS_ANALYSER_PROMPT = """
+You are very good at analysing biases and you are tasked with analyzing the text to identify potential biases.
 
 Here is the text input : {text}
 
-Please focus on identifying biases related to race, profession, gender, and value. If no biases are detected, categorize the text as "no-bias." 
+Please focus on identifying biases related to culture,race, profession, gender, disabilities,value  and religion . If no biases are detected, categorize the text as "no-bias." 
 
 For each document, provide the analysis in the format outlined below. 
 
-Bias Status: Indicate the type of bias detected, which could be one of the following: race, profession, gender, value,cultural or no-bias.
+Bias Status: Indicate the type of bias detected, which could be one of the following: race, profession, gender,disabilities, value,cultural or no-bias.
 
 Reason: Offer a concise explanation for why the text was categorized with a particular bias status, citing specific examples or language from the text.
 
@@ -20,4 +20,4 @@ Instructions:
 3. Deliver a justification grounded in specific evidence from the text.
 
 4. Keep the analysis clear, objective, and succinct.
-"
+"""
