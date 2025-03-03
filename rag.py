06b66279c6262f0284269db21e099b39a6350d62
Shilpi2525 <shilpi.shaw@pyxeda.ai>
2025-03-03 16:35:59 +0530

    Update rag.py

--- rag.py
+++ rag.py
@@ -35,9 +35,28 @@ def extract_text_from_pdf(pdf_path):
 
 #Function
 
-def generate_text_from_image():
+#def generate_text_from_image():
     
+response = client.chat.completions.create(
+    model="gpt-4o-mini",
+    messages=[
+        {
+            "role": "user",
+            "content": [
+                {"type": "text", "text": "What's in this image?"},
+                {
+                    "type": "image_url",
+                    "image_url": {
+                        "url": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg",
+                    },
+                },
+            ],
+        }
+    ],
+    max_tokens=300,
+)
 
+print(response.choices[0])
 
 
 #Function 
