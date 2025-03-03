d6fd9241f49ddba2cc5cf8435d441b328ef9259a
Shilpi2525 <shilpi.shaw@pyxeda.ai>
2025-03-03 17:14:10 +0530

    Update rag.py

--- rag.py
+++ rag.py
@@ -71,31 +71,6 @@ def extract_text_from_image(image_path:str):
     print(response.choices[0])
 
 
-#Function
-def extract_text_from_image_url():    
-    response = client.chat.completions.create(
-    model="gpt-4o-mini",
-    messages=[
-        {
-            "role": "user",
-            "content": [
-                {"type": "text", "text": "What's in this image?"},
-                {
-                    "type": "image_url",
-                    "image_url": {
-                        "url": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg",
-                    },
-                },
-            ],
-        }
-    ],
-    max_tokens=300,
-    )
-
-    print(response.choices[0])   
-    
-
-
 
 #Function 
 def bias_analyser(text: str):
