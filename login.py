19cb467391729051dad89e355d0a2f4830ece72f
Shilpi2525 <shilpi.shaw@pyxeda.ai>
2025-03-04 15:45:32 +0530

    Update login.py

--- login.py
+++ login.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import authlib
+from app import chat
 
 IMAGE_ADDRESS = "https://europe.ippf.org/sites/europe/files/styles/header_background_xs/public/2022-12/illustration-14-website-no-logos-no-text.jpg?itok=y-vJsgkS"
 
@@ -15,6 +16,7 @@ else:
     st.html(f"Hello, <span style='color: orange; font-weight: bold;'>{st.experimental_user.name}</span>!")
     if st.button("Log out", type="secondary", icon=":material/logout:"):
         st.logout()
+    chat()
     
 st.caption(f"Streamlit version {st.__version__}")
 st.caption(f"Authlib version {authlib.__version__}")
