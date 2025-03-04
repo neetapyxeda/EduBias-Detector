d54781e59e585b5bef734c0692baffb8198ca52b
Shilpi2525 <shilpi.shaw@pyxeda.ai>
2025-03-04 15:51:58 +0530

    Update login.py

--- login.py
+++ login.py
@@ -10,7 +10,7 @@ st.title("EduBias Detector")
 st.image(IMAGE_ADDRESS)
 
 if not st.experimental_user.is_logged_in:
-    if st.button("Log in with Google", type="primary", icon=":material/login:"):
+    if st.sidebar.button("Log in with Google", type="primary", icon=":material/login:"):
         st.login()
 else:
     st.html(f"Hello, <span style='color: orange; font-weight: bold;'>{st.experimental_user.name}</span>!")
