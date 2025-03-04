b4bf09e4c958367b755fc147d111e33d13eb35c0
Shilpi2525 <shilpi.shaw@pyxeda.ai>
2025-03-04 16:29:18 +0530

    Update login.py

--- login.py
+++ login.py
@@ -17,9 +17,9 @@ if not st.experimental_user.is_logged_in:
 
 else:
     st.html(f"Hello, <span style='color: orange; font-weight: bold;'>{st.experimental_user.name}</span>!")
-    if st.button("Log out", type="secondary", icon=":material/logout:"):
+    if st.sidebar.button("Log out", type="secondary", icon=":material/logout:"):
         st.logout()
     chat()
     
-st.caption(f"Streamlit version {st.__version__}")
-st.caption(f"Authlib version {authlib.__version__}")
+#st.caption(f"Streamlit version {st.__version__}")
+#st.caption(f"Authlib version {authlib.__version__}")
