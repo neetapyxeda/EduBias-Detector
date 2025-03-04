8575c2c5f5ce7a064301d30c5cb58f8e1e6b9e6f
Shilpi2525 <shilpi.shaw@pyxeda.ai>
2025-03-04 16:05:45 +0530

    Update login.py

--- login.py
+++ login.py
@@ -12,6 +12,7 @@ st.image(IMAGE_ADDRESS)
 if not st.experimental_user.is_logged_in:
     if st.sidebar.button("Log in with Google", type="primary", icon=":material/login:"):
         st.login()
+
 else:
     st.html(f"Hello, <span style='color: orange; font-weight: bold;'>{st.experimental_user.name}</span>!")
     if st.button("Log out", type="secondary", icon=":material/logout:"):
